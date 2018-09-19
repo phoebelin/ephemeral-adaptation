7fc2b28dbd7fbcd586e2a35725736ea233723bbf
Phoebe Lin <phoebe.lin93@gmail.com>
2018-09-19 17:15:02 -0400

    working on zipf distribution

--- javascripts/ephemeral.js
+++ javascripts/ephemeral.js
@@ -15,7 +15,7 @@ var getRandomNum = function(max) {
 
 var word_groups = [["Braise", "Saute", "Roast", "Grill"],["Muscle", "Tendon", "Ligament", "Cartilage"],["Crawfish", "Shrimp", "Prawn", "Lobster"],["Mussel", "Oyster", "Scallop", "Abalone"],["Ceramic", "Granite", "Porcelain", "Marble"],["Blender", "Mixer", "Juicer", "Chopper"],["Fridge", "Stove", "Microwave", "Dishwasher"],["Doctor", "Physician", "Surgeon", "Nurse"],["Carpet", "Vinyl", "Laminate", "Hardwood"],["Hemlock", "Spruce", "Cedar", "Juniper"],["Happy", "Scared", "Mellow", "Angry"],["Winter", "Summer", "Spring", "Autumn"],["Arctic", "Atlantic", "Pacific", "Indian"],["Hearts", "Spades", "Diamonds", "Clubs"],["Clock", "Timepiece", "Sundial", "Watch"],["Canucks", "Flames", "Oilers", "Senators"],["Basil", "Oregano", "Thyme", "Parsley"],["Samsun", "Sanyo", "Panasonic", "Pioneer"],["Hershey", "Caramilk", "Smarties", "Eatmore"],["Tahiti", "Samoa", "Tonga", "Tuvalu"],["France", "Germany", "Spain", "England"],["Gucci", "Armani", "Versace", "Vuitton"],["Embroidery", "Crochet", "Knitting", "Sewing"],["Sardine", "Trout", "Salmon", "Whitefish"],["Bigfoot", "Sasquatch", "Minotaur", "Ogopogo"],["Flannel", "Linen", "Cotton", "Spandex"],["Scrabble", "Checkers", "Chess", "Backgammon"],["Almond", "Pecan", "Pistachio", "Walnut"],["Cheddar", "Camembert", "Gouda", "Parmesan"],["Molson", "Kokanee", "Labatt", "Coors"],["Ballroom", "Ballet", "Swing", "Hiphop"],["Beethoven", "Brahms", "Chopin", "Handel"],["Violin", "Flute", "Piano", "Percussion"],["Rhine", "Amazon", "Danube", "Yangtze"],["Penny", "Nickel", "Quarter", "Dollar"],["Purse", "Backpack", "Schoolbag", "Duffle"],["Terrier", "Bulldog", "Dalmatian", "Poodle"],["Stroll", "Saunter", "Prance", "Swagger"],["Hydrogen", "Helium", "Oxygen", "Nitrogen"],["Saturn", "Jupiter", "Venus", "Mercury"],["Sneaker", "Sandal", "Moccasin", "Loafer"],["Toyota", "Mazda", "Mercedes", "Honda"],["Termite", "Katydid", "Spider", "Ladybug"],["Coupe", "Minivan", "Sedan", "Hatchback"],["London", "Paris", "Madrid", "Berlin"],["Bahamas", "Barbados", "Jamaica", "Bermuda"],["Japan", "China", "Korea", "Singapore"],["Lipstick", "Nailpolish", "Shadow", "Blush"],["Herbal", "Rooibos", "Jasmine", "Peppermint"],["Virgo", "Taurus", "Aquarius", "Gemini"],["Chardonnay", "Merlot", "Shiraz", "Cabernet"],["Reebok", "Asics", "Adidas", "Converse"],["Tornado", "Cyclone", "Hurricane", "Storm"],["Poplar", "Birch", "Alder", "Willow"],["Spoon", "Knife", "Spatula", "Ladle"],["Lasagna", "Spaghetti", "Linguine", "Penne"],["Movie", "Theatre", "Musical", "Opera"],["Shirt", "Jacket", "Sweater", "Overcoat"],["Pencil", "Ballpoint", "Marker", "Crayon"],["House", "Apartment", "Cabin", "Cottage"],["Squirrel", "Mouse", "Hamster", "Gerbil"],["Tiger", "Leopard", "Cheetah", "Cougar"],["Parrot", "Bluebird", "Robin", "Budgie"],["Paperback", "Magazine", "Newspaper", "Journal"],["Roman", "Byzantine", "Egyptian", "Ottoman"],["Horror", "Comedy", "Drama", "Foreign"],["Hockey", "Skiing", "Curling", "Skating"],["Mountain", "Knoll", "Highland", "Foothill"],["Carrot", "Potato", "Onion", "Eggplant"],["Airplane", "Helicopter", "Blimp", "Balloon"],["Kayak", "Gondola", "Canoe", "Sailboat"],["Painting", "Sculpture", "Portrait", "Photograph"],["Soccer", "Basketball", "Baseball", "Football"]]
 var selected_word_groups = [];
-var words_for_tasks = [[], [], []]
+var words_for_tasks = [];
 
 var index = getRandomNum(72);
 var menu_counter = -1;
@@ -53,8 +53,42 @@ $(document).ready(function() {
   var start_time = date.getTime();
   var end_time = start_time;
 
-  createTask();
+  //CREATING MENUS HERE
   createMenus();
+
+  for (var x = 0; x < selected_word_groups.length; x += 16) {
+
+    words_for_tasks.push(selected_word_groups.slice(x, x+16));
+    
+  }
+
+  //randomly choose 8 items from each menu used
+  //then use zipf distribution to generate task selection sequence
+  //console.log(words_for_tasks);
+  var selected_prompts = [];
+  var createPrompt = function() {
+    for (var x = 0; x < 3; x++) {
+      for (var y = 0; y < 8; y++) {
+        var random_index = Math.floor(Math.random() * 15);
+        while (selected_prompts.includes(words_for_tasks[x][random_index])) {
+          random_index = Math.floor(Math.random() * 15);
+        }
+        selected_prompts.push(words_for_tasks[x][random_index]);
+      }
+    }
+
+    var sliced_array = []
+    for (var x = 0; x < selected_prompts.length; x += 8) {
+      sliced_array.push(selected_prompts.slice(x, x+8));
+    }
+    //console.log(sliced_array);
+
+    
+
+  }
+
+  //CREATING PROMPT HERE
+  createPrompt();
 
   $("#menu1").on("click", function (event) {
     var item = document.getElementById("menu1dropdown");
@@ -107,14 +141,10 @@ $(document).ready(function() {
   })
 
 
-  for (var x = 0; x < selected_word_groups.length; x++) {
-    
-    for (var y = 0; y < 3; y++) {
-      words_for_tasks[y].push(selected_word_groups[x]);
-    }
-    
-  }
-  console.log(words_for_tasks);
+  
+
+ 
+  
 
 });
 
@@ -167,21 +197,9 @@ var getWord = function() {
 
 }
 
-console.log("selected words groups: " + selected_word_groups);
+console.log(words_for_tasks);
 
-//randomly choose 8 items from each menu used
-//then use zipf distribution to generate task selection sequence
-var createTask = function() {
-  var offset = 16;
-  for (var menu = 0; menu < 3; menu++) {
-    for (var x = 0; x < 8; x++) {
-      var random_index = Math.floor(Math.random()*15);
 
-    }
-
-  }
-
-}
 
 
 $(".dropdown_item").click(function(e) {
